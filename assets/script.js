9c23afbe09f8f75b0ce621f51cb078ea8be658b9
unknown <jacob.jasperson@yahoo.com>
2022-10-19 22:41:39 -0700

    added html, js, and css sheets and added some formatting

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var searchFormEl = document.getElementById("widget")
+var searchButtonEl = document.getElementById("search-button")
+var cityEl = document.getElementById("city")
+var searchQuery = document.getElementById("search-query")
+var weatherCard = document.getElementById("weather-card")
+
