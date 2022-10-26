7a9ba83e52242b26c715191034c10df635bb0711
unknown <jacob.jasperson@yahoo.com>
2022-10-25 23:38:36 -0700

    added to js, stuck on local storage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# weather
+# 5 Day Weather Forecast
+
+## Description
+
+My goal for this project was to build a web app that would receive the user input for a city and then generate a 5 day forecast of the weather for that city. I built this project to put into practice the use of web APIs, local storage, html, css, and javascript. This project is a work in progress.
+
+## Usage
+
+Input a city into the search bar and receive a 5 day weather forecast for that city.
+
+![screenshot](assets/images/Capture.PNG)
+
+## License
+
+MIT license
--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,65 @@
 var searchFormEl = document.getElementById("widget")
 var searchButtonEl = document.getElementById("search-button")
 var cityEl = document.getElementById("city")
-var searchQuery = document.querySelector("#search-query")
+
 var weatherCard = document.getElementById("weather-card")
 var APIKey = "2d2d2544f1c9ad441e1cf2f84814182e"
-var citySearchHistory = document.querySelector("ul")
+var citySearchHistory = document.querySelector("#city-search-history")
+var cityStorage = ["1"]
 
 //search form requiring user input
 function handleSearch(event) {
     event.preventDefault();
 
-    var city = document.querySelector("#search-query").value;
-
+    var city = document.querySelector("#search-input").value;
+    cityStorage.push(city);
+    console.log(cityStorage);
     if (!city) {
         console.error("You need a city input value!");
         return;
     }
-    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey
+    // var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey
+    console.log(city);
+    // getApi(queryURL);
 
-location.assign(queryString);
+    localStorage.setItem("city", JSON.stringify(cityStorage))
+    console.log(cityStorage.length)
+    renderButtons();
 }
 
-searchButtonEl.addEventListener("submit", handleSearch);
+function loadDataFromLocalStorage() {
+    var localStorageData = JSON.parse(localStorage.getItem("city"));
 
-function getApi() {
-       
-    fetch(queryURL)
-        .then(function(response) {
-        return response.json();
-         })
-         .then(function(data) {
-        for (var i = 0; i < data.length; i++) {
-            var cityList = document.createElement("btn")
-            cityList.textContent = data[i].//searched variable
-            citySearchHistory.appendChild(cityList);
+    cityStorage = localStorageData;
 
-        }
-            console.log(data);
-         })
+    renderButtons();
 }
+loadDataFromLocalStorage();
+
+function renderButtons() {
+        citySearchHistory.innerHTML = "";
+    for (var i = 0; i < cityStorage.length; i++) {
+        var searchHistory = document.createElement("button")
+        citySearchHistory.appendChild(searchHistory);
+        searchHistory.textContent = cityStorage[i];
+    }
+}
+searchButtonEl.addEventListener("click", handleSearch);
+
+// function getApi(queryURL) {
+
+//     fetch(queryURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             for (var i = 0; i < data.length; i++) {
+//                 var cityList = document.createElement("btn")
+//                 cityList.textContent = data[i].name//searched variable
+//                     console.log(cityList);
+//                 citySearchHistory.appendChild(cityList);
+
+//             }
+//             console.log(data);
+//         })
+// }
